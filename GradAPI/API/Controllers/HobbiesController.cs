39da75e87f7992395bc49f76d72ff51f08c21bdc
PauloN <Paulon@bbd.co.za>
2022-06-09 10:32:23 +0200

    Fixed invalid id for delete and update

--- GradAPI/API/Controllers/HobbiesController.cs
+++ GradAPI/API/Controllers/HobbiesController.cs
@@ -44,15 +44,20 @@ namespace API.Controllers {
 
         [HttpPut("edit/{id}")]
         public IActionResult UpdateHobby(int id, Hobbies Hobby) {
-             Hobbies _hobby = _context.Hobbies.GetById(id);
+          if(!int.TryParse(id.ToString(), out id))
+                return BadRequest("Invalid Id presented");
+              
             try{
-                 
+                 Hobbies _hobby = _context.Hobbies.GetById(id);
+                if(_hobby == null){
+                    return BadRequest("User with specified id not found!");
+                }
                   _hobby.Name = Hobby.Name;
                   _hobby.Description = Hobby.Description;
                   _context.Hobbies.Update(_hobby);
 
                  if (_context.Hobbies.SaveChanges() > 0) 
-                 return StatusCode(200, "Updated Hobby: '"+ Hobby.Name+ "' Successful!");
+                    return StatusCode(200, "Updated Hobby: '"+ Hobby.Name+ "' Successful!");
               }
               catch{
                   return StatusCode(500, "Failed to Update Hobby: '"+ Hobby.Name + "'");
@@ -62,16 +67,21 @@ namespace API.Controllers {
         
         [HttpDelete("delete/{id}")]
         public IActionResult DeleteHobby(int id) {
-              Hobbies _hobby = _context.Hobbies.GetById(id);
+            if(!int.TryParse(id.ToString(), out id))
+                return BadRequest("Invalid Id presented");
+              
             try{
-                
+                Hobbies _hobby = _context.Hobbies.GetById(id);
+                if(_hobby == null){
+                    return BadRequest("User with specified id not found!");
+                }
                   _context.Hobbies.Delete(_hobby);
 
                  if (_context.Hobbies.SaveChanges() > 0) 
                  return StatusCode(200, "Delete Hobby: '"+ _hobby.Name+ "' Successful!");
               }
               catch{
-                  return StatusCode(500, "Failed to Delete Hobby: '"+ _hobby.Name + "'");
+                  return StatusCode(500, "Failed to Delete Hobby");
               }
            return NoContent();
         }
